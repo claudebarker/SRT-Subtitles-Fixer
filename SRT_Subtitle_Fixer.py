c3bfa189c47d2f131606d6934fcda03c0e80c6ff
Megalon <ebr2v73+u84ejo@sharklasers.com>
2014-02-08 22:45:57 -0800

    v0.6.0 Option to insert new subtitle. Other Fixes.
    + Added option to insert a blank, 0 length subtitle at specified time in
    00:00:00,000 format
    + Added additional comments and help
    - Removed superfluous method

--- SRT_Subtitle_Fixer.py
+++ SRT_Subtitle_Fixer.py
@@ -1,5 +1,5 @@
 """ 
-Version 0.5.1
+Version 0.6.0
 Written in python 2.7.6 by Megalon
 
 This is a simple command line program to delay or speed up the timing of a ".srt" subtitle file.
@@ -57,33 +57,24 @@ def convertToSRTTimeCode(time):
 
 # Method to convert to milliseconds, then apply the time offset, then convert back to SRT timecode.
 def modifyTime(time, offset, startTime):
-	time = convertToMilliseconds(time)
-	if convertToMilliseconds(startTime) < time:
+	if startTime < time:
 		time = time + offset
-	time = convertToSRTTimeCode(time)
 	return time
 
-# Method to modify the lines from the input file. Called for every line with SRT timecode.
-def modifyLine(line, offset, startTime):
+# Method to split the timing line into beginning and end variables
+def splitLine(line):
 	# The starting time for this subtitle text
-	start = line[0:12]
+	beginning = line[0:12]
 	# The ending time for this subtitle text
 	end = line[-13:]
-	
-	# Call the method to change the time for the start and end times
-	start = modifyTime(start, offset, startTime)
-	end = modifyTime(end, offset, startTime)
-	
-	# Return the full string, with both the start and end times, in the correct format
-	return start + " --> " + end + "\n"
+	return beginning, end
 	
 def main():
 	parser = argparse.ArgumentParser(description="\
 	------------------------------------------------------------------------------\n \
-	 ------------------------- SRT Subtitle Fixer v0.5.1 --------------------------\n \
+	------------------------- SRT Subtitle Fixer v0.6.0 --------------------------\n \
 	------------------------------------------------------------------------------\n \
-	 -- Alter the timing of an SRT subtitle file. ---------------------------------\n \
-	 -- Example: SRT_Subtitle_Fixer.py C:\\Input.srt C:\\Output.srt \n\
+	Example: SRT_Subtitle_Fixer.py C:\\Input.srt C:\\Output.srt \n\
 	00:00:05,000 -5000 \
 	------------------------------------------------------------------------------")
 	parser.add_argument('inputPath', metavar='inputPath', default=None, help='Input file path')
@@ -91,15 +82,18 @@ def main():
 	parser.add_argument('startTime', default='00:00:00,000', help='When to begin applying the offset to the srt timecodes. Default is 00:00:00,000')
 	parser.add_argument('delayTime', help='How much time, in milliseconds, to offset the timing in the srt file. Positive, or negative.')
 	parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output. Prints every change in SRT timecode.')
+	parser.add_argument('-i', '--insert', action='store', help='Insert a blank subtitle at specified time in 00:00:00,000 format.')
 	args = parser.parse_args()
 	inputPath = args.inputPath
 	outputPath = args.outputPath
 	startTime = args.startTime
 	offset = int(args.delayTime)
-	
+	if args.insert != None:
+		insertTime = convertToMilliseconds(args.insert)
 	# The startTime can also be entered as 0, so the user does not have to enter 00:00:00,000 each time
-	if startTime == '0':
+	if startTime == '0' or startTime == 0:
 		startTime = '00:00:00,000'
+	startTime = convertToMilliseconds(startTime)
 	
 	print "\nDelaying subtitles " + inputPath + " by " + str(offset) + " milliseconds, and writing to file " + outputPath
 	
@@ -108,20 +102,46 @@ def main():
 	
 	# Variable that holds the number of subtitles in the document.
 	subtitlesCount = -1
-	# Boolean that tells if the next line is a counting line that tells the subtitle number.
+	# Boolean that tells if the next line has the subtitle number.
 	nextCount = True
+	previousTime = 0
 	
 	for line in input:
+		# If the line has a subtitle number, increate the subtitle count, and print it to the file.
 		if nextCount == True:
 			subtitlesCount = subtitlesCount + 1
 			output.write(str(subtitlesCount) + "\n")
 			nextCount = False
-		# Check if the line is a timing line. 00:00:00,000 If not, simply write the line to the output file.
+		# Check if the line is a timing line. 00:00:00,000 format. If not, simply write the line to the output file.
 		# 30 characters is the lenght of the timing lines
 		elif len(line) == 30:
-			if line[2] == ':' and line[5] == ':' and line[8] == ',':
-				# Call the method to modify this line to the new time, then write to the output file
-				modifiedLine = modifyLine(line, offset, startTime)
+			if line[2] == ':' and line[5] == ':' and line[8] == ',':				
+				# Split the line into beginning time and end time
+				beginning, end = splitLine(line)
+				
+				# Convert beginning and end time to milliseconds
+				beginning = convertToMilliseconds(beginning)
+				end = convertToMilliseconds(end)
+				
+				# Attempt to insert a new line if the option is chosen
+				if args.insert != None:
+					# Check if the insertTime is after the previous time, but before the current time
+					# If so, insert a new blank subtitle, and increase the subtitle count
+					if previousTime < insertTime and beginning > insertTime:
+						subtitlesCount = subtitlesCount + 1
+						insertedLine = convertToSRTTimeCode(insertTime) + " --> " + convertToSRTTimeCode(insertTime) + "\n"
+						output.write(insertedLine + "\n" + str(subtitlesCount) + "\n")
+					previousTime = beginning
+				
+				# Call the method to change the time for the beginning and end times
+				beginning = modifyTime(beginning, offset, startTime)
+				end = modifyTime(end, offset, startTime)
+				
+				beginning = convertToSRTTimeCode(beginning)
+				end = convertToSRTTimeCode(end)
+				
+				modifiedLine = beginning + " --> " + end + "\n"
+				
 				# Check for the verbose option
 				if args.verbose:
 					print subtitlesCount
@@ -135,7 +155,7 @@ def main():
 		else:
 			output.write(line)
 	print "Done!"
-	print "Delayed subtitles " + inputPath + " by " + str(offset) + " milliseconds, starting at " + startTime + " and wrote to file " + outputPath + "!"
+	print "Delayed subtitles " + inputPath + " by " + str(offset) + " milliseconds, starting at " + convertToSRTTimeCode(startTime) + " and wrote to file " + outputPath + "!"
 		
 if __name__ == "__main__":
 	main()
